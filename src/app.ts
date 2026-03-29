8c589062027ad57a41bc659bf72950345e07ec11
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-28 23:09:18 -0500

    connect health and event routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import healthRoutes  from "./api/v1/routes/healthRoutes";
+import cors from "cors";
+import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
+import setupSwagger from "./config/swagger";
+import { helmetMiddleware } from "./config/helmetOptions";
+import { getCorsOptions } from "./config/corsOptions";
 
-export const app = express();
+const app = express();
 
 app.use(express.json());
+app.use(helmetMiddleware);
+app.use(cors(getCorsOptions()));
+
+setupSwagger(app);
 
 app.use("/api/v1/health", healthRoutes);
-app.use("/api/v1", eventRoutes);
+app.use("/api/v1/events", eventRoutes);
+
+export default app;
